d022fc485049a0c0c4ebdd931d652a81be81a4d4
YunDaYoung <ydiosa98@gmail.com>
2020-09-14 05:45:36 +0900

    #2.0 Developing MainPage

---
+++ app/src/main/java/com/example/linkb/LoginedMainPage.java
@@ -0,0 +1,14 @@
+package com.example.linkb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginedMainPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logined_main_page);
+    }
+}
---
+++ app/src/main/java/com/example/linkb/roundImageView/RoundImageView.java
@@ -0,0 +1,41 @@
+package com.example.linkb.roundImageView;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Path;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
+
+public class RoundImageView extends androidx.appcompat.widget.AppCompatImageView {
+
+    public static float radius = 30.0f;
+
+    public RoundImageView(Context context) {
+        super(context);
+    }
+
+    public RoundImageView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public RoundImageView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public void setRectRadius(Float radius){
+        this.radius = radius;
+        invalidate();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        Path clipPath = new Path();
+        RectF rect = new RectF(0, 0, this.getWidth(), this.getHeight());
+        clipPath.addRoundRect(rect, radius, radius, Path.Direction.CW);
+        canvas.clipPath(clipPath);
+        super.onDraw(canvas);
+    }
+}
